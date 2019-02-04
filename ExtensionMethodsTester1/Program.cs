6fafb8ee1314e470876e3647bd5b58d534afdc41
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-02-04 17:50:43 +0100

    Kilak przykadów

--- ExtensionMethodsTester1/Program.cs
+++ ExtensionMethodsTester1/Program.cs
@@ -10,6 +10,28 @@ namespace ExtensionMethodsTester1
     {
         static void Main(string[] args)
         {
+            string number = "3";
+            int n = Extensions.ToInt1(number);
+            int n2 = number.ToInt();
+            
+
+            Console.WriteLine(n2);
+
+            string text = "Maria lubi koty st dfg h fghfg hf gh fgh";
+
+            text.Log();
+            try
+            {
+                throw new Exception("Nie używaj tej aplikacji");
+            }
+            catch (Exception exe)
+            {
+                exe.Message.Log();                
+            }
+            string left = text.Left(5);
+            Console.WriteLine(left);
+
+            Console.Read();
         }
     }
 }
