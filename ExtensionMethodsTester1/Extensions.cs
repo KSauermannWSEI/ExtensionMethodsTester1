eef9d371410476ce85ea8b80fbb625016010de2d
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-02-05 05:01:27 +0100

    Komentarze

--- ExtensionMethodsTester1/Extensions.cs
+++ ExtensionMethodsTester1/Extensions.cs
@@ -9,7 +9,7 @@ namespace ExtensionMethodsTester1
 {
     public static class Extensions
     {
-        public static int ToInt(this String val)
+        public static int ToInt(this String val) //Przykład metody konwertujacej string na int
         {
             try
             {
@@ -21,22 +21,20 @@ namespace ExtensionMethodsTester1
             }
         }
 
-        public static int ToInt<T>(this T val) where T: struct
+        public static int ToInt<T>(this T val) where T : struct //Przykladowa metoda rozszerzająca generyczna
         {
             try
             {
                 Type type = typeof(T);
 
-                if (type == typeof(string))
+                if (type == typeof(string))//Sprawdzenie typu
                 {
                     return -100; //Dla testu
                 }
-                if (val is double)
+                if (val is double)//Sprawdzenie typu
                 {
-                    int x = int.Parse(val.ToString());
-                    //int z = val as int?;
-
-                    return Convert.ToInt32(x + 1);
+                    int x = int.Parse(val.ToString()); //Rzuci wyjątkiem jeśli dostanie liczbę z przecinkiem.
+                    return Convert.ToInt32(x + 1); //nie rzuci wyjatkiem jeśli dostanie liczbę z przecinkiem -> zostawi tylko część całkowitą 
                 }
                 return Convert.ToInt32(val);
             }
@@ -46,7 +44,7 @@ namespace ExtensionMethodsTester1
             }
         }
 
-        public static int ToInt1(String val)
+        public static int ToInt1(String val) //Zwykla metoda konwertująca
         {
             try
             {
@@ -57,26 +55,21 @@ namespace ExtensionMethodsTester1
                 return -1;
             }
         }
-        public static string Left(this string val, int digits)
+        public static string Left(this string val, int digits) //Przykladowa mtoda podobna to funkcji z Excela
         {
             return val.Substring(0, digits);
         }
-        public static void Log(this string text)
+        public static void Log(this string text) //Przyklad bez parameru
         {
-            try
-            {
-                string directory = "D:/Loggs";
-                File.AppendAllText(Path.Combine(directory, "Info.txt"),
-                    $"{Environment.NewLine} Data: {DateTime.Now.ToShortDateString()} Informacja: {text}");
-            }
-            catch (DirectoryNotFoundException dnfe)
-            {
-               // LogError(dnfe.Message);
-            }
-            catch (Exception)
-            {
-               // LogError("Coś się zepsuło");
-            }
+            string directory = "D:/Loggs";
+            File.AppendAllText(Path.Combine(directory, "Info.txt"),
+                $"{Environment.NewLine} Data: {DateTime.Now.ToShortDateString()} Informacja: {text}");
+        }
+        public static void Log(this string text, string fileName) //Przykład z parametrem
+        {
+            string directory = "D:/Loggs";
+            File.AppendAllText(Path.Combine(directory, fileName),
+                $"{Environment.NewLine} Data: {DateTime.Now.ToShortDateString()} Informacja: {text}");
         }
     }
 }
--- ExtensionMethodsTester1/Program.cs
+++ ExtensionMethodsTester1/Program.cs
@@ -8,34 +8,38 @@ namespace ExtensionMethodsTester1
 {
     class Program
     {
-        class Roman
+        class Roman //Inner class -> zalezność calosciowa agregacja/kompozycja, gdzie klasa Roman nie może istnieć bez klasy Program
         {
             public int MyProperty { get; set; }
         }
         static void Main(string[] args)
         {
-            string number = "3";
-            int n = Extensions.ToInt1(number);
-            int n2 = number.ToInt();
+            string number = "3"; // Przykladowy ciąg który będziemy konwertować na int.
+            int n = Extensions.ToInt1(number); // Użycie zwykłej metody do konwersji.
+            int n2 = number.ToInt(); // Użycie metody rozszerzającej do konwersji. 
 
-            double d = 43.4d;
-            int dd = d.ToInt();           
+            double d = 43.4d; 
+            int dd = d.ToInt(); //Użycie metody rozszerzającej generycznej          
 
             Console.WriteLine(dd);
 
-            //string text = "Maria lubi koty st dfg h fghfg hf gh fgh";
+            string text = "Maria lubi koty st dfg h fghfg hf gh fgh";
 
-            //text.Log();
-            //try
-            //{
-            //    throw new Exception("Nie używaj tej aplikacji");
-            //}
-            //catch (Exception exe)
-            //{
-            //    exe.Message.Log();                
-            //}
-            //string left = text.Left(5);
-            //Console.WriteLine(left);
+            text.Log(); //Użycie metody rozszerzającej do logowania tekstu na dysk.
+
+            //Przkład z logowaniem blędów
+            try
+            {
+                throw new Exception("Nie używaj tej aplikacji");
+            }
+            catch (Exception exe)
+            {
+                exe.Message.Log("Error.txt");
+            }
+
+            //Uzycie metody Left gdzie podajemy ile znaków od lewej strony ma byc zwrócone ze stringa
+            string left = text.Left(5);
+            Console.WriteLine(left);
 
             Console.Read();
         }
